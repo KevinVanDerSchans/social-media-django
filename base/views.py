a06b28b0c413b23494bfdb3bc512fa6b4a68a5bf
Kevin Schans <kevinvdsd@hotmail.com>
2024-05-28 23:49:59 +0200

    Add delete room_message functionality

--- base/views.py
+++ base/views.py
@@ -131,4 +131,17 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
 
-    return render(request, 'base/delete.html', {'obj':room})
+    return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not the owner...')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj': message})
