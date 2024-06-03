c2e5ae2ab80c696b4d7dac4d85f1f7498bca767c
Kevin Schans <kevinvdsd@hotmail.com>
2024-06-03 22:28:10 +0200

    Add login_register template

--- base/views.py
+++ base/views.py
@@ -15,24 +15,23 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
-
         else:
-            messages.error(request, 'Username or password does not exist', context)
+            messages.error(request, 'Username OR password does not exit')
 
-    context={'page': page}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
