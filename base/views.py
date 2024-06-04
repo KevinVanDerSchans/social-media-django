756234e0dbe091a869ee5548ec0a8012e89c5f25
Kevin Schans <kevinvdsd@hotmail.com>
2024-06-04 20:52:57 +0200

    Update register form with MyUserCreationForm

--- base/views.py
+++ base/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 def loginPage(request):
     page = 'login'
@@ -39,10 +38,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
