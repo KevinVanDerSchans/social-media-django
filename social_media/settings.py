499990e53f0111cab42614ee0e934f2fb1d4ba9f
Kevin Schans <kevinvdsd@hotmail.com>
2024-06-04 20:31:12 +0200

    Config and prepare avatar and change image functionality

--- social_media/settings.py
+++ social_media/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
@@ -113,14 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-# STATIC_ROOT =
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
+# STATIC_ROOT =
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
